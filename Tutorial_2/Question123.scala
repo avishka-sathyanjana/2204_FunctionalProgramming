f1b32061d1aa24ed3df432240b949d75434076a9
Avishka Sathyanjana <90499124+avishka-sathyanjana@users.noreply.github.com>
2022-07-10 21:05:26 +0530

    Question123

---
+++ Tutorial_2/Question123.scala
@@ -0,0 +1,24 @@
+object question123 extends App{
+    
+    //declaring variables
+
+    var k, i, j = 2;
+    var m, n = 5;
+
+    var f = 12.0f;
+    var g = 4.0f;
+
+    var c = 'X';
+
+    //evaluating and displaying expressions
+    println("a) Result = " + (k + 12 * m));
+    println("b) Result = " + (m / j));
+    println("c) Result = " + (n % j));
+    println("d) Result = " + (m / j * j));
+    println("e) Result = " + (f + 10 * 5 + g));
+
+    i += 1;
+
+    println("f) Result = " + (i * n));
+
+}
