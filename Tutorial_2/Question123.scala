b5cd731fd9d6ed68330a7f0c00d24ef9767a981e
Avishka Sathyanjana <90499124+avishka-sathyanjana@users.noreply.github.com>
2022-07-10 22:06:50 +0530

    add q3part2

--- Tutorial_2/Question123.scala
+++ Tutorial_2/Question123.scala
@@ -11,11 +11,11 @@ object question123 extends App{
     var c = 'X';
 
     //evaluating and displaying expressions
-    println("a) Result = " + (k + 12 * m));
-    println("b) Result = " + (m / j));
-    println("c) Result = " + (n % j));
-    println("d) Result = " + (m / j * j));
-    println("e) Result = " + (f + 10 * 5 + g));
+    println("a) Result = " + (k+12*m));
+    println("b) Result = " + (m/j));
+    println("c) Result = " + (n%j));
+    println("d) Result = " + (m/j*j));
+    println("e) Result = " + (f+10*5+g));
 
     i += 1;
 
---
+++ Tutorial_2/Question3part2.scala
@@ -0,0 +1,37 @@
+object question3part2 extends App{
+
+    //declaring ints 
+    var a:Int = 2;
+    var b:Int = 3;
+    var c:Int = 4;
+    var d:Int = 5;
+
+    //declaring float
+    var k:Float = 4.3f;
+    var g:Float = 4.0f;
+
+
+    //a
+    b-=1;
+    println("a) Result = "+ (b*a+c*d)) ;
+    d-=1;
+
+    //b
+    println("b) Result = "+ (a)) 
+    a+=1;
+
+    //c
+    println("c) Result = "+ (-2*(g-k)+c)) ;
+
+    //d
+    println("d) Result = "+ (c));
+    c+=1;
+
+    c+=1;
+    c=c*a;
+    a+=1;
+
+    println("e) Result = "+ (c))
+
+
+}
